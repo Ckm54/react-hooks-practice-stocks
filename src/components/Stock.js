a63c8f11ca9cd9111ee302f4756bd08b574e0ed6
Collins <mutaic84@gmail.com>
2022-05-08 09:18:49 +0300

    create a stock click handler

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-function Stock({ stock }) {
+function Stock({ stock, handleStockClick }) {
+
+  function handleSale() {
+    handleStockClick(stock)
+  }
   return (
     <div>
-      <div className="card">
+      <div className="card" onClick={handleSale}>
         <div className="card-body">
           <h5 className="card-title">{stock.name}</h5>
           <p className="card-text">{stock.price}</p>
